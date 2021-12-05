8a07ecc57563d7074d107f2bc63134ef3720459c
syncrase <pierre.taquet@gmail.com>
2021-12-05 16:21:29 +0100

    Initial version of plantsMS generated by generator-jhipster@7.4.1

---
+++ jhiRoot/plantsMS/src/main/java/fr/syncrase/ecosyst/config/Constants.java
@@ -0,0 +1,11 @@
+package fr.syncrase.ecosyst.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
